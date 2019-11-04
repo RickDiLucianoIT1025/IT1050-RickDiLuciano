23777ab36d0f2702849658ebebe91bb101d624c4
Rick DiLuciano <drickja@yahoo.com>
2019-11-04 17:32:27 -0500

    Lab 1 New

---
+++ Lab01/Lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string A;
+            string B;
+            int a;
+            int b;
+
+
+
+            Console.WriteLine("Enter first integer");
+            A = Console.ReadLine();
+            a = Convert.ToInt32(A);
+            Console.WriteLine("Enter second integer");
+            B = Console.ReadLine();
+            b = Convert.ToInt32(B);
+
+
+            Console.WriteLine(a + "x" + b + "=" + a * b);
+            Console.ReadLine();
+ 
+        }
+    }
+}
