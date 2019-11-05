f955dcf70408c1e27c874dbcc6a7d34e4b5356d2
Rick DiLuciano <drickja@yahoo.com>
2019-11-04 19:44:53 -0500

    Lab 2 Hello World and Answers

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine( "{0}\n{1}", "Hello World!", "From Rick DiLuciano");
+
+            Console.WriteLine("{0}\t{1}", "Hello World!", "From Rick DiLuciano");
         }
-    }
+    } // A. C# console application starts with the main method.  
+    // B. Integer does not have a decimal point.  Can be expressed binarily.  Floating point can not be expressed in binary terms and has a decimal point.
+    // C. Some examples of methods in Chapter 4 are Main, Static, Console.Writeline and Display Message. 
+    // D. Get accessor.  Set accessor.
+    // E. Class is what objects belong to.  Classes are used in methods.  Objects are members of a class.  Four classes can be created in C#.
+    // Static - provides a blueprint.  Can't be instantiated.
+    // Abstract - used to define common behavior.
+    // Partial - takes parts from a class and makes multiple files out of the files.
+    // Sealed - can't be inherited.
 }
